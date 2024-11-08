4a1b2467c8035e9f3fd36cd60d3eef649fdc3471
sivaprasadtg <80467976+sivaprasadtg@users.noreply.github.com>
2024-11-08 08:50:43 +0100

    Update exercise3.py
    Remove debugging print statement.
    Add comments for readability.

--- hv/exercise/exercise3.py
+++ hv/exercise/exercise3.py
@@ -1,12 +1,17 @@
+# a fun exercise
 s=""
 a=['t', 'a', 'c', '1', 'q', 'd', 'e']
 for i in range(4):
+	# this part checks modulus operand on i with 2 and 3 together
 	if(i%2==0 and i%3==0):
 		s += a[i]
+	# here only odd values of i will be considered
 	if(i%2 ==1):
 		s += a[(-abs(i-2)) **1]
-		print((-abs(i-2)) **1)
+	# in this part, block is executed when i is 2
 	if(i==2):
 		s += a[int((len(a)-1)/2)]
+
+# finally when for loop ends, this revers indexing appends the value to s
 s += a[-3]
 print(s)
